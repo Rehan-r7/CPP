490e57fdd269aad9bea5afcd86106807ca3fc26d
Rehan khan <111696261+Rehan-r7@users.noreply.github.com>
2023-04-29 23:35:17 +0530

    Create pyramid10.cpp

---
+++ Pattern/pyramid10.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int n;
+cin>>n;
+cout<<"Inverted Left-Half (number) :- "<<endl;
+for(int i = 1;i<=n;i++){
+    for (int k = 1; k <=i-1; k++)
+    {
+        cout<<" ";    
+    }
+   
+    for(int j = 1;j<=n-i+1;j++)
+    {
+       cout<<i;
+    }
+   
+      cout<<endl;
+}
+  return 0;
+}    
