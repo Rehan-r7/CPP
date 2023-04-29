596ba547deec0fa07d931fdb23cd26af462d86bc
Rehan khan <111696261+Rehan-r7@users.noreply.github.com>
2023-04-29 23:31:32 +0530

    Create pyramid8.cpp

---
+++ Pattern/pyramid8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int n;
+cin>>n;
+cout<<"Inverted Right-Half :- "<<endl;
+for(int i = 1;i<=n;i++){
+    for(int j = 1;j<=n-i+1;j++)
+    {
+       cout<<"*";
+    }
+      cout<<endl;
+}
+  return 0;
+}    
